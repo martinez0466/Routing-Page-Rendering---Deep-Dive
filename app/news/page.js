f8aeef520e41c5f328eb7ab943aff5ebb99d970b
martinez0466 <camilo93.martinez@gmail.com>
2026-02-16 17:57:15 +0100

    initial commit: add project structure and main header

---
+++ app/news/page.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function NewsPage() {
+    return (
+        <>
+            <h1>News Page</h1>
+            <ul className="news-list">
+                <li>
+                    <Link href="/news/first-new">First News Item</Link>
+                </li>
+                <li>
+                    <Link href="/news/second-news">Second News Item</Link>
+                </li>
+                <li>
+                    <Link href="/news/third-news">Third News Item</Link>
+                </li>
+            </ul>
+        </>
+    );
+}
