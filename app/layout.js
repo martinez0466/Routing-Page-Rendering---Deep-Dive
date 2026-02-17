d5a8b90d9b3fd6a3d241b8f9189b0b4ded1d9f1d
martinez0466 <camilo93.martinez@gmail.com>
2026-02-17 10:33:16 +0100

    chore: initial project-wide formatting with prettier

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,20 @@
-import MainHeader from "@/components/main-header";
+import MainHeader from '@/components/main-header';
 import './globals.css';
 
 export const metadata = {
-    title: 'Next.js Page Routing & Rendering', description: 'Learn how to route to different pages.',
-}
+  title: 'Next.js Page Routing & Rendering',
+  description: 'Learn how to route to different pages.',
+};
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-        <body>
+  return (
+    <html lang="en">
+      <body>
         <div id="page">
-        <MainHeader/>
-        {children}
+          <MainHeader />
+          {children}
         </div>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
--- app/news/[slug]/not-found.js
+++ app/news/[slug]/not-found.js
@@ -1,8 +1,8 @@
 export default function NewsNotFoundPage() {
-    return (
-        <div id="error">
-            <h1>Not Found!</h1>
-            <p>TUnfortunately, we could not find the requested page.</p>
-        </div>
-    );
+  return (
+    <div id="error">
+      <h1>Not Found!</h1>
+      <p>TUnfortunately, we could not find the requested page.</p>
+    </div>
+  );
 }
